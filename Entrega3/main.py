b3f0e0f5cc1854afe75db2034f33a0adb0d8c8be
tanevitch <lubatane@gmail.com>
2022-05-03 15:53:13 -0300

    obj props

--- Entrega3/main.py
+++ Entrega3/main.py
@@ -3,11 +3,14 @@ from rdflib import Graph, Literal, RDF, RDFS, URIRef, OWL, Namespace
 from rdflib.namespace import FOAF , XSD
 
 BASE_URL = Namespace("http://www.semanticweb.org/")
+BASE_SCHEMAORG_URL = Namespace("https://schema.org/")
 
 g = Graph()
+g.bind("schema", BASE_SCHEMAORG_URL)
+g.bind("sw", BASE_URL)
 
-def count_individuals_of(node_type: str):
-    return len(list(g.triples((None, RDF.type, BASE_URL[node_type]))))
+def count_individuals_of(node_type: str, url= BASE_URL):
+    return len(list(g.triples((None, RDF.type, url[node_type]))))
 
 def add_individual(node_type, label):
     
@@ -24,7 +27,7 @@ def add_actor(movie, actor):
             "Actor",
             actor.get("name")
         )
-    g.add((movie, BASE_URL["actor"], actor))
+    g.add((movie, BASE_SCHEMAORG_URL["actor"], actor))
     g.add((actor, BASE_URL["playsIn"], movie))
 
 def add_genre(movie, genre):
@@ -34,10 +37,17 @@ def add_genre(movie, genre):
         )))
     
 def add_director(movie, director):
-    g.add((movie, BASE_URL["director"], add_individual(
+    g.add((movie, BASE_SCHEMAORG_URL["director"], add_individual(
             "Director",
             director.get("name")
         )))
+
+def add_rating(movie, rating):
+    rating_individual= BASE_URL["aggregateRating"+str(count_individuals_of("AggregateRating", url=BASE_SCHEMAORG_URL))]
+
+    g.add((rating_individual, RDF.type, BASE_SCHEMAORG_URL["AggregateRating"]))
+    g.add((rating_individual, BASE_URL["ratingValue"], Literal(rating.get("ratingValue"))))
+    g.add((movie, BASE_SCHEMAORG_URL["aggregateRating"], rating_individual))
 
 def add_movie(movie):
     movie_individual= add_individual(
@@ -45,9 +55,8 @@ def add_movie(movie):
         movie.get("name")
     )
     
-    g.add((movie_individual, XSD.duration, Literal(movie.get("duration"))))
-    g.add((movie_individual, XSD.dateTime, Literal(movie.get("datePublished"))))
-    
+    g.add((movie_individual, BASE_URL["duration"], Literal(movie.get("duration")))) if movie.get("duration") else None
+    g.add((movie_individual, BASE_SCHEMAORG_URL["dateCreated"], Literal(movie.get("datePublished"), datatype=BASE_SCHEMAORG_URL["Date"]))) if movie.get("datePublished") else None
 
     for actor in movie.get("actor") or []:
         add_actor(movie_individual, actor)
@@ -58,6 +67,8 @@ def add_movie(movie):
         
     for director in movie.get("director") or []:
         add_director(movie_individual, director)
+
+    add_rating(movie_individual, movie.get("aggregateRating"))
 
 with open('mergeado.json', encoding='utf-8') as fh:
     json_peliculas = json.load(fh)
