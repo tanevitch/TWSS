0d743738a6e4e389d136cf203f9149a2de1fb8de
tanevitch <lubatane@gmail.com>
2022-05-13 21:32:32 -0300

    base url generica

--- Entrega4/main.py
+++ Entrega4/main.py
@@ -3,11 +3,12 @@ from rdflib import Graph, Literal, RDF, RDFS, URIRef, OWL, Namespace
 from rdflib.namespace import FOAF , XSD
 import argparse
 import requests
-import re 
+import re
+
+from sameas_generator import BASE_URL 
 
 G = Graph()
 SAMEAS_G= Graph()
-BASE_URL = Namespace("http://www.semanticweb.org/")
 BASE_SCHEMAORG_URL = Namespace("https://schema.org/")
 BASE_DBPEDIA_URL = Namespace("http://dbpedia.org/property/") 
 G.bind("dbp", BASE_DBPEDIA_URL)
@@ -15,13 +16,11 @@ G.bind("dbp", BASE_DBPEDIA_URL)
 def search_required_properties(actor_triples, actor_uri):
     actor_graph= Graph()
     actor_graph.parse(data=actor_triples)
-        
+  
     triples_to_add= []
-
     for s,o,p in actor_graph.triples((actor_uri, BASE_DBPEDIA_URL["birthDate"] ,None)):
         triples_to_add.append([s,o,p])
 
-    
     for s,o,p in actor_graph.triples((actor_uri,  BASE_DBPEDIA_URL["occupation"] ,None)):
         triples_to_add.append([s,o,p])
     
@@ -31,11 +30,10 @@ def get_resource_url(obj):
     return SAMEAS_G.value(subject=obj, predicate=OWL.sameAs)
 
 def get_ttl_url(obj):
-    
     formats = requests.get(get_resource_url(obj)).headers["Link"].split(",")
     for f in formats:
         if "ttl" in f:
-            return re.compile(r"http([\s\S]+).ttl").search(f).group().replace("<","").replace(">", "")
+            return re.compile(r"http([\s\S]+).ttl").search(f).group().replace("<","").replace(">", "").replace("https", "http")
 
 def search_actors():
     for s,o,p in G.triples((None, BASE_SCHEMAORG_URL["actor"], None)):
@@ -51,25 +49,22 @@ def search_actors():
             G.add((s,p,o))
 
 
-
-
 def dbpedia_search():
     search_actors()
 
 def load_movies(input_file):
     G.parse(input_file, format='ttl', encoding="utf-8")
+    global BASE_URL
+    BASE_URL = Namespace(G.value(predicate=RDF.type,object=OWL.Ontology)+"/")
     SAMEAS_G.parse("links.ttl", format='ttl', encoding="utf-8")
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()                                               
-    # parser.add_argument("--input", "-i", type=str, required=True)
-    # parser.add_argument("--output", "-o", type=str, required=True)
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()                                               
+    parser.add_argument("--input", "-i", type=str, required=True)
+    parser.add_argument("--output", "-o", type=str, required=True)
+    args = parser.parse_args()
 
-    # load_movies(args.input)
-    # dbpedia_search()
-
-    # G.serialize(args.output, format="ttl", encoding="utf-8")
-    load_movies("dataset-original.ttl")
+    load_movies(args.input)
     dbpedia_search()
-    G.serialize("dataset-enriquecido.ttl", format="ttl", encoding="utf-8")
+
+    G.serialize(args.output, format="ttl", encoding="utf-8")
--- Entrega4/sameas_generator.py
+++ Entrega4/sameas_generator.py
@@ -5,12 +5,13 @@ from rdflib.namespace import FOAF , XSD
 
 G = Graph()
 G.parse("dataset-original.ttl", format='ttl', encoding="utf-8")
-BASE_URL = Namespace("http://www.semanticweb.org/")
+
+BASE_URL = Namespace(G.value(predicate=RDF.type,object=OWL.Ontology)+"/")
 BASE_SCHEMAORG_URL = Namespace("https://schema.org/")
 
 def search_sameAs():
     g = Graph()
-    dbr= Namespace("https://dbpedia.org/resource/")
+    dbr= Namespace("http://dbpedia.org/resource/")
     g.bind("dbr", dbr)
     g.bind("sw", BASE_URL)
     for s,o,p in G.triples((None, BASE_SCHEMAORG_URL["actor"], None)):
